d01dbc56759ab664a539cfde83b11c26e6e85106
Jon Andoni Galdos <jonandonigv@gmail.com>
2023-03-06 16:52:18 +0100

    Create a show chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -17,7 +18,7 @@ class ChirpController extends Controller
 
         return Inertia::render('Chirps/Index', [
 
-            //
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
 
         ]);
     }
@@ -33,9 +34,15 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
